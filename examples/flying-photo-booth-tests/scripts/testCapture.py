65bc2503e342182ce4e303c2663c7099bb228d35
Benedict Lau <ben.hy.lau@gmail.com>
2014-09-23 01:13:41 -0400

    Updated base test case class APIs and sample test script

--- examples/flying-photo-booth-tests/scripts/testCapture.py
+++ examples/flying-photo-booth-tests/scripts/testCapture.py
@@ -47,71 +47,55 @@ class CaptureTestCase(YkspTestCase):
         '''
         Tests capturing of a single picture.
         '''
-        # Start the main Activity and save the screen after one second
-        self.startActivity('LaunchActivity')
+        # Launch the app from the launcher
+        self.launchApp()
+
+        # Save the screen after one second [saveScreen() or refreshScreen() must be called to update the view tree after each screen transition]
         self.saveScreen('single-capture-before', sleep=1)
 
-        # Try to find the view containing the text 'PHOTO 1 OF 2' and fail the test if unsuccessful
-        try:
-            self.vc.findViewWithTextOrRaise(TEXT_BEFORE_CAPTURE)
-        except ViewNotFoundException:
-            self.fail('Failed to find view with text: %s' % TEXT_BEFORE_CAPTURE)
+        # Find the view containing the text 'PHOTO 1 OF 2' [raise an exception to fail the test if unsuccessful]
+        self.vc.findViewWithTextOrRaise(TEXT_BEFORE_CAPTURE)
 
-        # Try to find the view containing the text 'CAPTURE' and fail the test if unsuccessful
-        captureButton = None
-        try:
-            captureButton = self.vc.findViewWithTextOrRaise(TEXT_CAPTURE_BUTTON)
-        except ViewNotFoundException:
-            self.fail('Failed to find view with text: %s' % TEXT_CAPTURE_BUTTON)
+        # Find the view containing the text 'CAPTURE' and click on the centre of the view
+        self.vc.findViewWithTextOrRaise(TEXT_CAPTURE_BUTTON).touch()
 
-        # Click on the centre of the view and save screen after five seconds
-        captureButton.touch()
+        # Save screen after five seconds
         self.saveScreen('single-capture-after', sleep=5)
 
-        # Try to find the view containing the text 'PHOTO 2 OF 2' and fail the test if unsuccessful
-        try:
-            self.vc.findViewWithTextOrRaise(TEXT_AFTER_CAPTURE)
-        except ViewNotFoundException:
-            self.fail('Failed to find view with text: %s' % TEXT_AFTER_CAPTURE)
+        # Find the view containing the text 'PHOTO 2 OF 2' to validate the successful capture
+        self.vc.findViewWithTextOrRaise(TEXT_AFTER_CAPTURE)
 
     def testSwipeDiscard(self):
         '''
         Tests the 'swipe discard' feature after capturing.
         '''
-        # Start the main Activity and save the screen after one second
-        self.startActivity('LaunchActivity')
-        self.saveScreen('swipe-discard-before', sleep=1)
+        # Launch the app from the launcher
+        self.launchApp()
 
-        # Try to find the view containing the text 'PHOTO 1 OF 2' and fail the test if unsuccessful
-        titleView = None
-        try:
-            titleView = self.vc.findViewWithTextOrRaise(TEXT_BEFORE_CAPTURE)
-        except ViewNotFoundException:
-            self.fail('Failed to find view with text: %s' % TEXT_BEFORE_CAPTURE)
+        # Refresh the screen after one second
+        self.refreshScreen(sleep=1)
 
-        # Try to find the view containing the text 'CAPTURE' and fail the test if unsuccessful
-        captureButton = None
-        try:
-            captureButton = self.vc.findViewWithTextOrRaise(TEXT_CAPTURE_BUTTON)
-        except ViewNotFoundException:
-            self.fail('Failed to find view with text: %s' % TEXT_CAPTURE_BUTTON)
+        # Find the view containing the text 'PHOTO 1 OF 2'
+        titleView = self.vc.findViewWithTextOrRaise(TEXT_BEFORE_CAPTURE)
+
+        # Find the view containing the text 'CAPTURE'
+        captureButton = self.vc.findViewWithTextOrRaise(TEXT_CAPTURE_BUTTON)
 
         # Get the centres of two views to be used for swipe gesture with a drag duration of 500 ms
         dragStartXY = titleView.getCenter()
         dragEndXY = captureButton.getCenter()
         dragDuration = 500
 
-        # Click on the centre of the capture button, followed by a discard gesture after one second, then save screen after three seconds
+        # Click on the centre of the capture button, followed by a discard gesture after one second
         captureButton.touch()
         self.vc.sleep(1)
         self.device.drag(dragStartXY, dragEndXY, dragDuration)
+
+        # Save screen after three seconds
         self.saveScreen('swipe-discard-after', sleep=3)
 
-        # Try to find the view containing the text 'PHOTO 1 OF 2' and fail the test if unsuccessful
-        try:
-            self.vc.findViewWithTextOrRaise(TEXT_AFTER_DISCARD)
-        except ViewNotFoundException:
-            self.fail('Failed to find view with text: %s' % TEXT_AFTER_DISCARD)
+        # Find the view containing the text 'PHOTO 1 OF 2' to validate successful discard
+        self.vc.findViewWithTextOrRaise(TEXT_AFTER_DISCARD)
 
 if __name__ == '__main__':
     YkspTestCase.main(sys.argv)
